769c1bf1138b8837933629bdf910ce2acb1b909c
Pierre <pixelstock@hotmail.fr>
2014-02-05 18:54:17 +0100

    ajout des nuages aléatoires et en continu

---
+++ src/game/elements/Nuages.java
@@ -0,0 +1,59 @@
+package game.elements;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import managers.RessourcesManager;
+
+public class Nuages {
+
+	public static final int NB_MAX_NUAGES = 21;
+	public static final int NB_VIDES = 10;
+
+	private ArrayList<Integer> sequence;
+	private ArrayList<Integer> hauteurs;
+
+	private int x0 = 0;
+	
+	private boolean depl = false;
+
+	private void initSequence (){
+		sequence = new ArrayList<Integer>();
+		for (int i=1;i<=NB_MAX_NUAGES;sequence.add(i++));
+		for (int i=0;i<10;sequence.add(0),i++);
+		Collections.shuffle(sequence);		
+		hauteurs = new ArrayList<Integer>();
+		for (int i=0;i<NB_MAX_NUAGES+NB_VIDES;hauteurs.add((int) (Math.random()*100)%50),i++);
+		System.out.println(sequence);
+	}
+
+	public Nuages (){
+		initSequence();
+	}
+
+	public void drawNuages (RessourcesManager rm){
+		int xl = x0,i=0;
+		while (xl < 1000 && i < sequence.size()){
+			int id = sequence.get(i);
+			if (id != 0){
+				int x = id%3, y = id/3;
+				rm.getImage("NUAGES").getSubImage(x*280, y*200, 280, 200).draw(xl,hauteurs.get(id));
+			}
+			xl+=280;
+			i++;
+		}
+	}
+	
+	public void update (){
+		if (depl){			
+			x0--;
+			if (x0 <= -280){
+				sequence.remove(0);
+				x0=0;
+			}
+			if (sequence.size() == 0)
+				initSequence();
+		}
+		depl=!depl;
+	}
+}
--- src/game/states/Level_A.java
+++ src/game/states/Level_A.java
@@ -1,5 +1,7 @@
 package game.states;
 
+import game.elements.Nuages;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -8,20 +10,23 @@ import org.newdawn.slick.state.StateBasedGame;
 public class Level_A extends AbstractLevel {
 
 	public static final int ID = 1;
+	
+	private Nuages nuages;
 
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
-		
+		nuages = new Nuages();
 	}
 
 	@Override
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
-		ressourcesManager.getImage("BG").draw(0,0);		
+		ressourcesManager.getImage("BG").draw(0,0);
+		nuages.drawNuages(ressourcesManager);
 	}
 
 	@Override
 	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
-		
+		nuages.update();
 	}
 
 	@Override
