acb364ad16961d4d4e544a3e8293350062a68850
Pierre <pixelstock@hotmail.fr>
2014-02-05 17:30:44 +0100

    lib + main + read me

---
+++ src/game/Main.java
@@ -0,0 +1,35 @@
+package game;
+
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+
+public class Main extends StateBasedGame {
+
+	private AppGameContainer container;
+
+	public Main () {
+		super("Mamad Quest : An Adventure in JohanLand");
+	}
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException
+	{
+		if (container instanceof AppGameContainer) {
+			this.container = (AppGameContainer) container;
+		}
+	}
+
+
+	public static void main(String[] args)
+	{
+		try{
+			AppGameContainer container = new AppGameContainer(new Main());
+			container.setDisplayMode(1000, 650, false);
+			container.setTargetFrameRate(60);
+			container.start();
+		}
+		catch (SlickException e) {e.printStackTrace();}
+	}
+}
